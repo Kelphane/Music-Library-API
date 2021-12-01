2dccfb7488c0111ed604a1187d41c5eaa8a53685
Dom B <domlamont@yahoo.com>
2021-12-01 09:37:34 -0800

    Added try and catch statements to each handler

--- index.js
+++ index.js
@@ -9,28 +9,40 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.listen(3000);
+app.listen(3002);
 
 app.get('/api/songs', (req, res) => {
     const allSongs = repoContext.songs.findAllSongs();
     return res.send(allSongs);
 });
 
-app.get('/api/songs/:id', (req, res) => {
-    const id = req.params.id;
-    const song = repoContext.songs.findSongById(id);
-    return res.send(song);
+app.get('/api/songs/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const song = await repoContext.songs.findSongById(id);
+        return res.send(song);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
-app.post('/api/songs', [validateSong], (req, res) => {
-    const newSong = req.body;
-    const addedNewSong = repoContext.songs.createSong(newSong);
-    return res.send(addedNewSong);
+app.post('/api/songs', [validateSong], async (req, res) => {
+    try {
+        const newSong = req.body;
+        const addedNewSong = await repoContext.songs.createSong(newSong);
+        return res.send(addedNewSong);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 app.put('/api/songs/:id', [validateSong], (req, res) => {
-    const id = req.params.id;
-    const propertyToUpdate = req.body;
-    const updatedSong = repoContext.songs.updateSong(id, propertyToUpdate);
-    return res.send(updatedSong);
+    try {
+        const id = req.params.id;
+        const propertyToUpdate = req.body;
+        const updatedSong = repoContext.songs.updateSong(id, propertyToUpdate);
+        return res.send(updatedSong);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
